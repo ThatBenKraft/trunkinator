2ae41f1a3797793a5b9b2251851b0d3f7569efe5
ThatBenKraft <ben.kraft@rcn.com>
2023-10-11 21:41:30 -0400

    OOP Refactor

--- main.py
+++ main.py
@@ -18,32 +18,63 @@ __status__ = "Prototype"
 # Defines constants
 NUM_ROLES = 5
 NUM_DAYS = 3
-SPACING = 10
+SPACING = 12
 DATA_PATH = "trunker_data.xlsx"
 
 
 class Show:
-    # print(list(data_frame.iloc[:, 0]))
+    """
+    Used for show information calculation and storage.
+    """
+
     def __init__(self, data_frame: pd.DataFrame) -> None:
         """
-        Creates Show object.
+        Creates Show object. Takes Pandas dataframe as input
         """
         self._data = data_frame
+        # Create role and roster storages
+        self._availabilities: dict[str, list[list[str]]] = {}
+        self._rosters: dict[str, list[list[str]]] = {}
+        # For each day
+        for day in self.get_day_names():
+            # Calculate availabilities
+            availabilities = [
+                self._calculate_availability(role, day)
+                for role in self.get_role_names()
+            ]
+            # Store availabilities
+            self._availabilities[day] = availabilities
+            # Initialize roster
+            self._rosters[day] = []
+            # Calculate rosters from availabilities
+            self._calculate_rosters(availabilities, day)
 
-    def get_roles(self) -> list[str]:
+    def get_role_names(self) -> list[str]:
         """
         Returns role names.
         """
         return list(self._data.columns)[1 : NUM_ROLES + 1]
 
-    def get_days(self) -> list[str]:
+    def get_day_names(self) -> list[str]:
         """
         Returns day names.
         """
         start_index = NUM_ROLES + 1
         return list(self._data.columns)[start_index : start_index + NUM_DAYS]
 
-    def get_availabile_trunkers(self, role: str, day: str) -> list[str]:
+    def get_availabilities(self, day: str) -> list[list[str]]:
+        """
+        Returns day availabilities.
+        """
+        return self._availabilities[day]
+
+    def get_rosters(self, day: str) -> list[list[str]]:
+        """
+        Returns day rosters.
+        """
+        return self._rosters[day]
+
+    def _calculate_availability(self, role: str, day: str) -> list[str]:
         """
         Returns list of trunkers available for role on day.
         """
@@ -52,90 +83,71 @@ class Show:
         # Returns with which trunkers have these availabilities
         return list(filter(None, availabilities * self._data["Trunker"]))
 
-    def compile_availabilities(self, day: str) -> list[list[str]]:
+    def _calculate_rosters(
+        self,
+        availabilities: list[list[str]],
+        day: str,
+        index: int = 0,
+        roster: list[str] = [],
+    ) -> None:
         """
-        Calculates and returns a list of trunkers available in roles on a specific
-        day.
+        Iterates through availabilities to find all possible rosters.
         """
-        # Get list of available Trunkers for each role
-        return [self.get_availabile_trunkers(role, day) for role in self.get_roles()]
+        # If index has reached the end of the roles list:
+        if index == NUM_ROLES:
+            # Add copy of roster to list
+            self._rosters[day].append(roster.copy())
+            return
+        # Iterates through roles:
+        for role_index, role in enumerate(availabilities):
+            # For each trunker in a role:
+            for trunker in role:
+                # Skips any trunker already in roster
+                if trunker in roster:
+                    continue
+                # Adds trunker to current roster
+                roster.append(trunker)
+                # Iterates to next role
+                self._calculate_rosters(
+                    availabilities[role_index + 1 :], day, index + 1, roster
+                )
+                # Removes from roster to try a different Trunker in same spot.
+                roster.pop()
 
 
-def schedule_shows() -> None:
+def print_shows() -> None:
     """
-    Runs main actions to schedule Trunkers into shows based on role and day
-    availabilities.
+    Loads data sheet and prints out Trunker availability and possible rosters.
     """
-    # sheet_name = input("Please enter show name (Data sheet name): ")
-    sheet_name = "Trash Mountain"
+    sheet_name = input("Please enter show name (data sheet name): ")
+    # sheet_name = "Trash Mountain"
     # Reads excel data into dictionary
     show = Show(pd.read_excel(DATA_PATH, sheet_name, skiprows=1))
     # For each day:
-    for day in show.get_days():
-        role_availabilities = show.compile_availabilities(day)
-        role_names = show.get_roles()
-        # Prints title bar, trunker availabilities, and footer
+    for day in show.get_day_names():
+        # Prints title bar
         print("\n" + f" {day.upper()} ".center(NUM_ROLES * SPACING, "="))
-        for role_name, available_trunkers in zip(role_names, role_availabilities):
-            print(role_name.upper().ljust(10), ", ".join(available_trunkers))
+        # Acquires and capitalizes role names
+        role_names = [name.upper() for name in show.get_role_names()]
+        # Prints availabilities
+        for role_name, available_trunkers in zip(
+            role_names, show.get_availabilities(day)
+        ):
+            print(role_name.ljust(10) + ", ".join(available_trunkers))
+        # Prints bottom bar
         print("=" * NUM_ROLES * SPACING + "\n")
+
         # Prints role titles
-        for role_name in role_names:
-            print(role_name.upper().center(SPACING), end="")
+        [print(role_name.center(SPACING), end="") for role_name in role_names]
         print("\n")
-        # Prints all possible rosters from availabilities
-        generate_rosters(role_availabilities)
 
+        # Prints rosters
+        for roster in show.get_rosters(day):
+            [print(trunker.center(SPACING), end="") for trunker in roster]
+            print("")
 
-def compile_availabilities(
-    show: Show,
-    day: str,
-) -> list[list[str]]:
-    """
-    Calculates and returns a list of trunkers available in roles on a specific
-    day.
-    """
-    role_availabilities: list[list[str]] = []
-    # For each role
-    for role in show.get_roles():
-        # Builds list of available trunkers
-        available_trunkers = show.get_availabile_trunkers(role, day)
-        # Print out availabilities for role
-        print(role.upper().ljust(10), ", ".join(available_trunkers))
-        # Add to availabilities
-        role_availabilities.append(available_trunkers)
-
-    return role_availabilities
-
-
-def generate_rosters(
-    role_availabilities: list[list[str]],
-    index: int = 0,
-    roster: list[str] = [],
-) -> None:
-    """
-    Iterates through availabilities to find all possible rosters.
-    """
-    # If index has reached the end of the roles list:
-    if index == NUM_ROLES:
-        # Print out combination
-        for trunker in roster:
-            print(trunker.center(SPACING), end="")
-        print("")
-        return
-    # Iterates through roles:
-    for role_index, role in enumerate(role_availabilities):
-        # For each trunker in a role:
-        for trunker in role:
-            # Skips any trunker already in roster
-            if trunker in roster:
-                continue
-            # Adds trunker to current roster
-            roster.append(trunker)
-            # Iterates to next role
-            generate_rosters(role_availabilities[role_index + 1 :], index + 1, roster)
-            roster.pop()
+    print("")
 
 
 if __name__ == "__main__":
-    schedule_shows()
+    print_shows()
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,23 +1,2 @@
-aders = list(availability_info.keys())
-    # # Separates headers into roles and days
-    # role_names, day_names = headers[:NUM_ROLES], headers[NUM_ROLES:]
-
-    # # For each day:
-    # for day in day_names:
-    #     # Prints title bar
-    #     print("")
-    #     print(f" {day.upper()} ".center(NUM_ROLES * SPACING, "="))
-    #     # Prints day availabilities
-    #     day_availabilities = compile_availabilities(
-    #         availability_info,
-    #         trunkers,
-    #         role_names,
-    #         day,
-    #     )
-    #     # Prints closer and role titles
-    #     print("=" * NUM_ROLES * SPACING, "\n")
-    #     for role_name in role_names:
-    #         print(role_name.upper().center(SPACING), end="")
-    #     print("\n")
-    #     # Prints all possible rosters from availabilities
-    #     generate_r
+):
+        #     print(trunker.center(SPACING) fo
